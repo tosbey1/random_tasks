865f79419e0ca4b1d400325a1bf98af14b913bcd
Blake Bertuccelli <blakebertuccelli@gmail.com>
2016-04-20 15:37:59 -0500

    New conditions

--- isExternal/isExternal.js
+++ isExternal/isExternal.js
@@ -3,7 +3,7 @@ $('a').each(function() {
     return link.hostname !== window.location.hostname;
   }
   var mailTo = $('a[href^=mailto]').length;
-  if (isExternal(this) && mailTo >=1) {
+  if ((isExternal(this) && !($(this).hasClass('external-false') || $(this).parent().parent().hasClass('external-false')) && mailTo >=1)) {
     return $(this).addClass('external fancybox')
       .append($('<i>').addClass('fa fa-external-link'));
   }
